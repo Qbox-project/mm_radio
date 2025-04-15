b9e7412d9d3b5e38a9f077bcc8cefd46b41af4fd
ChatDisabled <ChatDisabled@users.noreply.github.com>
2025-04-15 20:38:12 +0000

    chore: bump manifest version to v0.1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Master Mind'
-version '2.0.7'
+version '0.1.0'
 description 'A beautiful Radio Resource for FiveM'
 repository 'https://github.com/SOH69/mm_radio'
 
